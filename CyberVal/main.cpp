f50853d0a5d1ad3ce2dd233cfaac58bb25d1592d
Pinguu-dev <72171280+Pinguu-dev@users.noreply.github.com>
2022-03-26 00:11:45 +0100

    Update main.cpp

--- CyberVal/main.cpp
+++ CyberVal/main.cpp
@@ -663,7 +663,7 @@ void ASCDAVSDFASCXD()
 
 	if (initD3D(MyWnd))
 	{
-		LineOfSight = reinterpret_cast<decltype(LineOfSight)>(valBase + 0x4AB2B80); 
+		LineOfSight = reinterpret_cast<decltype(LineOfSight)>(valBase + 0x4ABBE60); 
 		_beginthreadex(0, 0, MainLoop, 0, 0, 0);
 	}
 
